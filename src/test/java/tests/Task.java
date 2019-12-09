509209cedb1487dba155e7338620f4d20fc36ded
Kailibinuer.Abuduaini <myEmail@Gmail.com>
2019-12-09 19:30:43 +0100

    add string

--- src/test/java/tests/Task.java
+++ src/test/java/tests/Task.java
@@ -1,8 +1,16 @@
 package tests;
 
 public class Task {
-  public static void main(String[] args){
-  System.out.println("hello world");
-    // added frim github
+  public static void main(String[] args) {
+      System.out.println("hello world");
+      // added frim github
+
   }
-}
+
+      public void hi(){
+          System.out.println("Hi from intellij");
+          
+      }
+
+  }
+
